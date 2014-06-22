c52c07626fa4032235e03893c4b1868b37ac0063
Leon Weber <leon@leonweber.de>
2014-06-22 18:38:22 +0200

    Remove useless stuff from turing-add.py

--- turing-add.py
+++ turing-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from turing import TuringMachine
-import pprint
+import logging
 
 a = "a"
 q0 = "q₀"
@@ -11,24 +11,22 @@ q3 = "q₃"
 q4 = "q₄"
 q5 = "q₅"
 qf = "qf"
+blank = "b̸"
 
-tape = [a, a, None, a]
+tape = [a, a, blank, a]
 
 transitions = {(q0, a, a, 1, q0),
-               (q0, None, None, 1, q1),
+               (q0, blank, blank, 1, q1),
                (q1, a, a, 1, q2),
-               (q1, None, None, 0, q2),
-               (q2, None, None, -1, q3),
+               (q1, blank, blank, 0, q2),
+               (q2, blank, blank, -1, q3),
                (q2, a, a, 1, q2),
-               (q3, a, None, -1, q4),
+               (q3, a, blank, -1, q4),
                (q4, a, a, -1, q4),
-               (q4, None, a, -1, q5),
+               (q4, blank, a, -1, q5),
                (q5, a, a, -1, q5),
-               (q5, None, None, 1, qf)}
+               (q5, blank, blank, 1, qf)}
 
-machine = TuringMachine(tape, transitions, q0, {qf})
-print(str(machine.tape))
-pprint.pprint(machine.transitions)
-print("running machine")
+machine = TuringMachine(tape, transitions, q0, {qf}, loglevel=logging.DEBUG)
 machine.run()
 print(machine.output())
